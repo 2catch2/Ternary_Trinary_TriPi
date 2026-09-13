c612f58f9b0c7a0732679690c5e6e0655c2d0cca
Robert Vincent Burks <treskrub@outlook.com>
2026-09-13 16:06:31 -0500

    Initialize _init_.py with clean imports
    Add initial imports for ALU, pipeline, and cache.

---
+++ src/_init_.py
@@ -0,0 +1,6 @@
+# Clear all previous definition blocks and replace them with these clean imports:
+from src.alu import TernaryALU
+from src.pipeline import TrinaryProcessorPipeline, TernaryAssemblyParser
+from src.cache import TernaryCache
+
+# Execute your validation checks as normal right below!
